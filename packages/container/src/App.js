d3384301525ad98cd7cebbe9d541327ced58543d
Srishailam <sriyd531@gmail.com>
2023-06-27 00:34:49 -0400

    adding routing and integrating all three mfes

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import React, { lazy, Suspense} from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import {
  StylesProvider,
  createGenerateClassName,
 } from '@material-ui/core/styles';
-import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
+import Progress from './components/Progess';
+
+const MarketingAppLazy = lazy(() => import('./components/MarketingApp'));
+const AuthAppLazy = lazy(() => import('./components/AuthApp'));
 
 const generateClassName = createGenerateClassName({
     productionPrefix: 'co',
@@ -17,7 +20,12 @@ export default () => {
             <StylesProvider generateClassName={generateClassName}>
             <div>
                 <Header />
-                <MarketingApp />
+                <Suspense fallback={<Progress />}>
+                    <Switch>
+                        <Route path="/auth" component={AuthAppLazy} />
+                        <Route path="/" component={MarketingAppLazy} />
+                    </Switch>
+                </Suspense>
             </div>
             </StylesProvider>
         </BrowserRouter>
